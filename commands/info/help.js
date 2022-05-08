970d49302ac885f0d1bd32f5221807da78924ce3
LachyLegend <lachylegend05@gmail.com>
2022-05-08 14:04:33 +1000

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -4,6 +4,18 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        message.reply("!leaderboard to see who has done the most assignments!, optional args - t1, t2")
+        for(let i = 0;i < args.length; i++){
+            let arg = args[i].toLowerCase();
+            arg = arg.replace("-", "");
+            if(arg == "leaderboard"){
+                message.channel.send("Returns leaderboard of classwork, alias: lb, optional args: t1, t2, create-roles, set-roles")
+            } 
+            if(arg == "status") {
+                message.channel.send("Get info about person in class, example: !status lachlan")
+            }           
+        }
+        if(args.length == 0){
+            message.channel.send("Commands: leaderboard, status, help\n!help <cmd> to show help for a specific command, e.g. !help status")
+        }
     }
 } 
