bcdc5d1ad10cfc903ea64ef48c934fe17995139a
LachyLegend <lachylegend05@gmail.com>
2022-05-08 12:30:17 +1000

    Fixed heap issue and can create roles
    There was a for loop that didn't increase i, I also made the algorithm much more optimized removing a for loop.

--- commands/info/leaderboard.js
+++ commands/info/leaderboard.js
@@ -1,6 +1,22 @@
 const puppeteer = require('puppeteer');
 const { LismLogin } = require("../../util/functions")
 
+const CreateRole = async (roleName, colour, perms, message) => {
+    if (message.guild.roles.cache.find(role => role.name == roleName)) {
+        //The role already exists
+        message.channel.send(roleName + " already exists!");
+        return;
+    }
+
+    message.guild.roles.create({
+        name: roleName,
+        color: colour,
+     })
+     .then(role => {
+        message.channel.send(`Role \`${role.name}\` created!`);
+     })
+     .catch(console.error); 
+}
 const getLeaderboard = async function(page, term_url){
     await page.goto(term_url);
     // TODO have a date arg for the leaderboard
@@ -30,6 +46,7 @@ module.exports = {
         var leaderboardString = "The all time leaderboard for 'assignments handed in' is: ";
         var allTerms = true;
         var setRoles = false;
+        var createRoles = false;
         var username_arr = [];
         URL = "https://moodle.oeclism.catholic.edu.au/course/recent.php?id=897";
 
@@ -53,9 +70,11 @@ module.exports = {
             if(arg == "setrole" || arg == "setroles"){
                 setRoles = true;
             }
+            if(arg == "createrole" || arg == "createroles"){
+                createRoles = true;
+            }
         }
 
-        
         await LismLogin(page, URL)
 
         if(allTerms) {
@@ -84,62 +103,100 @@ module.exports = {
         });
         
         const guild = await client.guilds.fetch('950154084441288724')
-
-        message.guild.roles.cache.each(role => {
-            // find safer way to check
-            // if(role.name != "reminder" && role.name != "@everyone" && role.name != "bump reminder" && role.name != "Lismore Buddy"){
-            if(role.name == "SDD KING" || role.name == "SDD EDLER" || role.name == "SDD KNIGHT" || role.name == "SDD SOLDIER" || role.name == "SDD SLACKER" ){
-                message.guild.members.cache.each(member => member.roles.remove(role))
-            }
-        })
-
-        for(let i = 0; i < sortedNamesCount.length;){
+        for(let i = 0; i < sortedNamesCount.length; i++){
             leaderboardString += "\n" + sortedNamesCount[i][0] + " : " + sortedNamesCount[i][1];
-            // console.log(sortedNamesCount[i])
-            if(setRoles && i < 4){
-                //loop through an I until you find one that has a smaller value, if value is greater than 1 can't give out king status 
-                let ii;
-                for(ii = 1; ii < (sortedNamesCount - i);ii++){
-                    if(sortedNamesCount[i][1] != sortedNamesCount[i+ii][1]){
-                        break;
-                    }
+        }
+
+        if (createRoles){
+            CreateRole("SDD KING", "#F83E0C", 0, message);
+            CreateRole("SDD ELDER", "#D9540B", 0, message);
+            CreateRole("SDD KNIGHT", "#F07900", 0, message);
+            CreateRole("SDD SOLDIER", "#D98C0B", 0, message);
+            CreateRole("SDD SLACKER", "#4A412A", 0, message);
+        }
+
+        if(setRoles) {
+            //caching all the roles to give out
+            const wantedRoles = [
+                message.guild.roles.cache.find(role => role.name === "SDD KING"),
+                message.guild.roles.cache.find(role => role.name === "SDD ELDER"),
+                message.guild.roles.cache.find(role => role.name === "SDD KNIGHT"),
+                message.guild.roles.cache.find(role => role.name === "SDD SOLDIER"),
+                message.guild.roles.cache.find(role => role.name === "SDD SLACKER")
+            ];
+            message.guild.roles.cache.each(role => {
+
+                // if(role.name != "reminder" && role.name != "@everyone" && role.name != "bump reminder" && role.name != "Lismore Buddy"){
+                if(role.name == "SDD KING" || role.name == "SDD ELDER" || role.name == "SDD KNIGHT" || role.name == "SDD SOLDIER" || role.name == "SDD SLACKER" ){
+                    message.guild.members.cache.each(member => member.roles.remove(role));
+                    //The wantedRoles.push(role) method doesn't work because it doesn't have an order unfortunately
+                    //message.guild.roles.cache.find(role => role.name === "SDD KING")
                 }
-                let cached_i = i;
-                for(ii; ii > 0; ii--){
-                    try {
-                            let user = message.guild.members.cache.find(member => member.nickname == sortedNamesCount[i][0].split(" ")[0]);
+            })
+
+            cached_i = 0;
+            for(let i = 0; i < sortedNamesCount.length; i++){
+                // caching them here to avoid regrabing stuff, readability
+                let userName = sortedNamesCount[i][0];
+                let result = sortedNamesCount[i][1];
+                if(i < 4){
+                    const user = message.guild.members.cache.find(member => member.nickname == userName.split(" ")[0]);
+                    //loop through an I until you find one that has a smaller value, if value is greater than 1 can't give out king status 
+                    //remove console logs later, just for debug purposes
+                    for(let ii = 1; ii < (sortedNamesCount.length - i);ii++){
+                        try {
                             switch (cached_i) {
                                 case 0:
-                                    //if there is only one leader
-                                    if(ii == 1 && i == 0){
-                                        user.roles.add(message.guild.roles.cache.find(role => role.name === "SDD KING"));
+                                    if(result != sortedNamesCount[i+ii][1] && ii == 1){
+                                        break;
                                     }
-                                    else{
-                                        user.roles.add(message.guild.roles.cache.find(role => role.name === "SDD ELDER"))
-                                    }
+                                    user.roles.add(wantedRoles[1]);
+                                    console.log(user.nickname + " gets the role: 1 " + wantedRoles[1].name)
                                     break;
                                 case 1:
-                                    user.roles.add(message.guild.roles.cache.find(role => role.name === "SDD ELDER"));
+                                    user.roles.add(wantedRoles[1]);
+                                    console.log(user.nickname + " gets the role: 1 " + wantedRoles[1].name)
                                     break;
                                 case 2:
-                                    user.roles.add(message.guild.roles.cache.find(role => role.name === "SDD KNIGHT"));
+                                    user.roles.add(wantedRoles[2]);
+                                    console.log(user.nickname + " gets the role: 2" + wantedRoles[2].name)
                                     break;
                                 case 3:
-                                    user.roles.add(message.guild.roles.cache.find(role => role.name === "SDD SOLDIER"));
+                                    user.roles.add(wantedRoles[3]);
+                                    console.log(user.nickname + " gets the role: 3" + wantedRoles[3].name)
                                     break;
                                 default:
-                                    console.log(user.name + " Doesn't get a role!")
+                                    console.log(user.nickname + " Doesn't get a role!")
+                                    break;
                             }
                             
                         }
                         catch (TypeError){
-                            console.log(sortedNamesCount[i][0].split(" ")[0] + " could not be found")
+                            console.log(userName.split(" ")[0] + " could not be found")
                         }
-                        i++;
-                }
+                        if(result != sortedNamesCount[i+ii][1]){
+                            if(ii == 1 && i == 0){
+                                user.roles.add(wantedRoles[0]);
+                                console.log(user.nickname + "gets the role: " + wantedRoles[0].name)
+                            }
+                            cached_i++;
+                        }                        
+                        break;
+                    }
 
+                }
+                if (i == sortedNamesCount.length - 1){
+                    try {
+                        const user = message.guild.members.cache.find(member => member.nickname == userName.split(" ")[0]);
+                        user.roles.add(wantedRoles[4])
+                    }
+                    catch (TypeError){
+                        console.log(userName.split(" ")[0] + " could not be found")
+                    }
+                }
             }
         }
+ 
         message.channel.send(leaderboardString)
     }
 } 
