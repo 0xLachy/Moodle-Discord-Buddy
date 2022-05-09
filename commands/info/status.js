8452831da3bd4e75f111edd7d8758ec72e8f9f2c
phoenix-31 <tom3.9.27.81.243@gmail.com>
2022-05-09 19:25:52 +1000

    Made status responses more fancy!
    I also restyled a bit. I will make it more efficient soon.

--- commands/info/status.js
+++ commands/info/status.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
-const { LismLogin } = require("../../util/functions")
+const {MessageEmbed} = require('discord.js');
+const {LismLogin} = require("../../util/functions")
 
 module.exports = {
     name: "status",
@@ -7,9 +8,9 @@ module.exports = {
     permissions: [],
     devOnly: false,
     run: async ({client, message, args}) => {
-        //change context id to be your own
+        //TODO make context id settable.
         var URL = "https://moodle.oeclism.catholic.edu.au/user/index.php?contextid=123980&id=896&perpage=26";
-        var inputName = args[0].toLowerCase();
+        var inputName = args.join(" ")
         classAmount = 26;
         //TODO add nickname through slash command
         const nicknames = {
@@ -32,35 +33,46 @@ module.exports = {
             arg = arg.replace("-", "");           
         }
 
-        //start browser
+        // Starts browser.
         const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
-        //Get past login screen
+        // Gets past login screen.
         await LismLogin(page, URL)
 
-        //Loop through each student to get correct one
+        // Loops through each student to get correct one.
         for(let i = 0; i < classAmount; i++){
 
             let username = await page.evaluate((sel) => {
                 return document.querySelector(sel).textContent;
-                }, `#user-index-participants-896_r${i}_c0 > a`);
-            let LCUserName = username.toLowerCase();
-            if (LCUserName == inputName || LCUserName.split(" ")[0] == inputName){
+            }, `#user-index-participants-896_r${i}_c0 > a`);
+
+            if (username.toLowerCase() == inputName.toLowerCase() || username.split(" ")[0].toLowerCase() == inputName.toLowerCase()){
                 let statusRole = await page.evaluate((sel) => {
                     return document.querySelector(sel).textContent;
-                    }, `#user-index-participants-896_r${i}_c1`);
+                }, `#user-index-participants-896_r${i}_c1`);
+
                 let statusGroup = await page.evaluate((sel) => {
                     return document.querySelector(sel).textContent;
-                    }, `#user-index-participants-896_r${i}_c2`);
+                }, `#user-index-participants-896_r${i}_c2`);
+
                 let statusOnline = await page.evaluate((sel) => {
                     return document.querySelector(sel).textContent;
-                    }, `#user-index-participants-896_r${i}_c3`);
-                let statusString = "**" + username + "**\t=>" + "\t**Roles**: " + statusRole + "\t**Groups**: " + statusGroup + "\t**Last Online**: " + statusOnline;
-                message.channel.send(statusString);
+                }, `#user-index-participants-896_r${i}_c3`);
+                
+                let statusEmbed = new MessageEmbed();
+                statusEmbed.setTitle(username)
+                statusEmbed.addFields(
+                    {name: "Roles", value: statusRole},
+                    {name: "Groups", value: statusGroup},
+                    {name: "Last Online", value: statusOnline}
+                )
+                
+                message.channel.send({embeds: [statusEmbed]})
+
                 break;
             }
-            else if(i == classAmount -1){
+            else if(i == classAmount - 1){
                 message.channel.send("Couldn't find person, did you spell their name correctly")
             }
 
