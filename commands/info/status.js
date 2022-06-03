4607e821a2e42c4b539b0dd275b6183725be54a6
LachyLegend <lachylegend05@gmail.com>
2022-06-03 19:45:09 +1000

    Status now supports filtering

--- commands/info/status.js
+++ commands/info/status.js
@@ -11,8 +11,11 @@ module.exports = {
         //TODO make context id settable.
         var URL = "https://moodle.oeclism.catholic.edu.au/user/index.php?contextid=123980&id=896&perpage=26";
         var inputNames = [];
-        var fuzz;
+        var fuzz = false;
+        var filter = false;
         var filterArg = "";
+        //maybe store everything apart from last online locally? And call -Update to update db
+        var participantInfo = []
         //move pointer to function location
         //#form_autocomplete_input-1653380416775
         classAmount = 26;
@@ -34,11 +37,13 @@ module.exports = {
             if(arg == "fuzz"){
                 fuzz = true;
             } 
+            //TODO: make one that returns the whole class in a table in order of last online
             else if(arg == "filter"){
                 //Increase the arg counter, then get filter, that way it doesn't become a name
                 i++;
+                filter = true;
                 // eg. To filter by role you go "Role:-Student"
-                filterArg = args[i].replace("-", " ");
+                filterArg = args[i].replace("-", "");
             }
             else{
                 inputNames.push(arg)
@@ -54,8 +59,9 @@ module.exports = {
                 }
             }
         }
-        // Starts browser.
-        const browser = await puppeteer.launch({ headless: false});
+        // Starts browser visible 
+        //const browser = await puppeteer.launch({ headless: false});
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
 
         // Gets past login screen.
@@ -67,107 +73,6 @@ module.exports = {
             if(inputNames.length == 0) {
                 inputNames.push("filtering");
             }
-
-            // TRIED SO MUCH F*CKING SH*T IT TOOK HOURS FFS
-            // for (let i = 0; i < 8; i++) {
-            //     await page.keyboard.press('Tab');  
-            // }
-            // parent div class ||||| d-inline-block position-relative
-            //await page.waitForSelector('.form-control');
-            //context.browser.find_element_by_css_selector("div.jqx-listitem-element[id^='listitem1innerListBox'] > span")
-        //    await page.click("div.d-inline-block position-relative[id^='form_autocomplete_input']");
-           // await page.click("[id^='form_autocomplete_input-']");
-
-        // await page.$eval('.form-control', e => e.value = 'text');
-        //    await page.evaluate((element) => { element.click(); }, text);
-            // form_input =  await page.evaluate((sel) => {
-            //     return document.querySelector(sel).textContent;
-            // }, `.form-control`);
-            // console.log(form_input);
-            // console.log(await page.evaluate('document.querySelector(".form_input").getAttribute("Placeholder")'))
-            // await page.click("input[id^='form_autocomplete_input']")
-           // await page.click("input")
-           //await page.focus('input');
-      
-           
-
-
-
-        //   await page.evaluate((element) => { element.click(); }, form_input);
-            // console.log("found selecetor")
-            // await page.click(".form-control");
-           // await page.focus('input[type="text"]' )
-           await page.waitForSelector("[id^='form_autocomplete_input']");
-        //    try{
-
-        //    }
-        //    catch(error){
-        //        console.log(error);
-        //        console.log("ERROR CLLLLLLLLLLAAAAALED")
-        //        await page.click("[id^='form_autocomplete_input']");
-        //        await page.keyboard.type(filterArg);
-        //    }
-
-           await page.click("[id^='form_autocomplete_input']");
-           await page.focus("[id^='form_autocomplete_input']");
-           await page.keyboard.type(filterArg);
-          // page.waitForTimeout("30000");
-           await page.keyboard.press('ArrowDown', {delay: 250});
-           await page.evaluate(() => {
-            // const filterBox = document.querySelector("[id^='form_autocomplete_input']");
-            // const example_options = filterBox.querySelectorAll('option');
-            // const selected_option = [...example_options].find(option => option.text == "filterArg");
-            console.log(document.querySelector("[id^='form_autocomplete_suggestions'] > li"));
-            // selected_option.selected = true;
-          });
-        //    await page.keyboard.press('ArrowDown')
-        //    await page.keyboard.press('ArrowDown')
-           //page.waitForTimeout("30000");
-          // await page.keyboard.press('Enter')
-            // try{ 
-            //page.waitForSelector("div > [id^='form_autocomplete_suggestions'] > li");
-            // await page.evaluate((filterArg) => {
-            // console.log("GOT INSIDE EVAL")
-            // const filterBox = document.querySelector("[id^='form_autocomplete_suggestions'][role='listbox']");
-            // const example_options = filterBox.querySelectorAll('li');
-            // console.log(filterBox.id)
-            // // it's basically finding the filter box that has all the roles and stuff then getting the element but the element doesn't exist on the page
-            // const selected_option = [...example_options].find(option => option.innerText == filterArg);
-            // console.log([...example_options])
-            // // [...example_options].find(option => option.innerText == filterArg).click();
-            // //[...document.querySelectorAll('.elements button')].find(element => element.textContent === 'Button text').click();
-            // //page.click([...example_options].find(option => option.innerText == filterArg))
-            // console.log(selected_option);
-            // filterBox.firstElementChild.click();
-            //selected_option.click()
-            //page.click("[id^='form_autocomplete_suggestions'] > li[role='option']")
-            //page.click(selected_option.sel);
-            //const filterBox = document.querySelector("[role='option']")
-            //filterBox.click();
-            // console.log(example_options + " IS EXAMPLE OPTIONS")
-            // console.log(filterArg + " is filter arg")
-            // console.log(filterBox[0] + " example options 0");
-            
-            // example_options[0].click();
-            //console.log(selected_option + " IS EXAMPLE SLECTION THING")
-            // selected_option.click();
-            // selected_option.selected = true;
-            // }, filterArg);
-        //    }
-                // await page.click("[id^='form_autocomplete_suggestions'] > [role='option']");
-            // catch(error){
-            //     console.log(error)
-            //     console.log("didn't find thing")
-            //     await Promise.all([
-            //         await page.keyboard.press('Enter'),
-            //         page.waitForNavigation()
-            //         ])
-            // }
-        //    unordedList = await page.evaluate((sel) => {
-        //     return document.querySelector(sel);
-        // }, `[id^='form_autocomplete_input']`);
-
-
         }
 
         for (let inputName in inputNames){
@@ -175,30 +80,64 @@ module.exports = {
             inputName = inputNames[inputName]
             // Loops through each student to get correct one.
             Classloop: for(let i = 0; i < classAmount; i++){
-                //just for testing
-                break Classloop;
                 let username = await page.evaluate((sel) => {
                     return document.querySelector(sel).textContent;
                     }, `#user-index-participants-896_r${i}_c0 > a`);
                 let LCUserName = username.toLowerCase();
                 if (LCUserName == inputName || LCUserName.split(" ")[0] == inputName 
-                || (fuzz && LCUserName.includes(inputName)) || (filterArg != "" && inputName == "filtering")){
+                || (fuzz && LCUserName.includes(inputName)) || (filterArg != "" && filter)){
 
                     //Getting the status for each type
                     let statusRole = await GetRole(page, i);
                     let statusGroup = await GetGroup(page, i);
                     let statusOnline = await GetLastOnStatus(page, i);
-                    
-                    let statusEmbed = new MessageEmbed();
-                    statusEmbed.setTitle(username)
-                    statusEmbed.addFields(
-                        {name: "Roles", value: statusRole},
-                        {name: "Groups", value: statusGroup},
-                        {name: "Last Online", value: statusOnline}
-                    )
-                    statusEmbed.setColor("#156385")
-                    
-                    message.channel.send({embeds: [statusEmbed]})
+                    if (filter){
+                        // get all people to be array of objects, once it is last person make sure there stats match
+                        //This object idea might be really cool to use normally!
+                        tempPersonObj = { 
+                            "username" : username,
+                            "Role" : statusRole,
+                            "Group" : statusGroup,
+                            "Online" : statusOnline
+                        }
+                        participantInfo.push(tempPersonObj);
+                       
+                        //get the correct filter terms and check if they match person
+                        var filterArr = filterArg.split(":", 2);
+                        var filterStatusType = filterArr[0]
+                        var filterStatusValue = filterArr[1]
+                        // console.log(filterStatusType + " : " + filterStatusValue + filterArr)
+                        if(tempPersonObj[filterStatusType] == filterStatusValue){
+                            SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message);
+                        }
+                        //implement who has been offline the longest
+                        else if(filterStatusType == "Online"){
+                           // console.log("got into online")
+                            switch(filterStatusValue.toLowerCase()) {
+                                case "now":                                  //it says secs in participants screen but is essentially now
+                                    if(tempPersonObj[filterStatusType].includes("sec")){
+                                        SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message);
+                                    }
+                                  break;
+                                case "hour":
+                                    //TODO: fix if it says 3 days 4 hours it will include that
+                                    if(tempPersonObj[filterStatusType].includes("hour") && tempPersonObj[filterStatusType].includes("min")){
+                                        SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message);
+                                    }
+                                  break;
+                                case "day":
+                                    if(tempPersonObj[filterStatusType].includes("1 day")){
+                                        SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message);
+                                    }
+                                    break;
+                                default:
+                                    console.log("couldn't find anyone with " + filterStatusValue);
+                              } 
+                        }
+                    }
+                    else {
+                        SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message);
+                    }
                     //change it to && after finished testing
                     if(!fuzz && filterArg == ""){
                         console.log("broke loop");
@@ -210,12 +149,25 @@ module.exports = {
                     message.channel.send(`Couldn't find person: ${inputName}, did you spell their name correctly`)
                 }
             }
-
+            console.log(participantInfo)
         }
        // browser.close();
         
     }
 } 
+function SendEmbedMessage(username, statusRole, statusGroup, statusOnline, message) {
+    let statusEmbed = new MessageEmbed();
+    statusEmbed.setTitle(username);
+    statusEmbed.addFields(
+        { name: "Roles", value: statusRole },
+        { name: "Groups", value: statusGroup },
+        { name: "Last Online", value: statusOnline }
+    );
+    statusEmbed.setColor("#156385");
+
+    message.channel.send({ embeds: [statusEmbed] });
+}
+
 // async function ApplyFilter(page, filterString){
 //     //clicking on filter box
 //     await page.click("#form_autocomplete_input-1653380416775");
