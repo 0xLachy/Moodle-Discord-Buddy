ff3aea26112c2175d587575f8126fc96cee47a6d
LachyLegend <lachylegend05@gmail.com>
2022-06-08 12:14:21 +1000

    getAssignment returns missing assignments for input name as embed message, looks great but is very slow

--- commands/info/getAssignment.js
+++ commands/info/getAssignment.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
+const {MessageEmbed} = require('discord.js');
 const { LismLogin } = require("../../util/functions");
-
+//TODO this all works fine and dandy, except it is VERY slow, it might be good to cache the stuff and then have a -update to update cache
 module.exports = {
     name: "getassignment",
     aliases: ["getass", "gs"],
@@ -15,30 +16,68 @@ module.exports = {
     devOnly: false,
 run: async ({client, message, args}) => {
     //write code here
-    const browser = await puppeteer.launch({headless: false});
-    const page = await browser.newPage();
+    // const browser = await puppeteer.launch({headless: false});
+    const browser = await puppeteer.launch();
 
-    URL = "https://moodle.oeclism.catholic.edu.au/course/recent.php?id=897";
+    const page = await browser.newPage();
+    //later, if no person is called, try use their discord nickname, otherwise tell them to provide person name
+    inputName = "lachlan";
+    //undefined (on purpose)
+    var assignmentObject;
+
+    T1URL = "https://moodle.oeclism.catholic.edu.au/course/recent.php?id=896";
+    T2URL = "https://moodle.oeclism.catholic.edu.au/course/recent.php?id=897";
+    T3URl = "https://moodle.oeclism.catholic.edu.au/course/recent.php?id=898";
+
     var filter = false;
     
+    
     //bypass loginscreen
-    await LismLogin(page, URL)
+    //the login URL doesn't really matter becuase getAssignments visits a different page anyways
+    await LismLogin(page, T2URL)
+    //TODO have one to just return assignments for a week
+
     for(let i = 0;i < args.length; i++){
         let arg = args[i].toLowerCase();
         arg = arg.replace("-", "");
-        if(arg == "filter" || arg == "f"){
+        if(arg == "u" || arg == "updatecache" || arg == "update"){
+            //TODO right code to update cache, maybe use mongo db for permanent cache
+        }
+        else if(arg == "filter" || arg == "f"){
             //instead call the function but as a filter
             filter = true;
         }
-        else if(arg == "all"){
-            //return all assignments for the term, maybe pass in term to get assignments
+        //might make this the defualt arg tbh 
+        else if(arg == "all"){   
+            assignmentObject = {...await GetAllAssignments(page, T1URL), 
+            ...await GetAllAssignments(page, T2URL) }
+        }//Getting by term
+        else if(arg == "t1" || arg == "term1"){
+            assignmentObject = await GetAllAssignments(page, T1URL)
+        }
+        else if(arg == "t2" || arg == "term2"){
+            assignmentObject = await GetAllAssignments(page, T2URL)
+        }
+        else if(arg == "t3" || arg == "term3"){
+            assignmentObject = await GetAllAssignments(page, T3URl)
         }
         else{
             //call the main function with the index and compare if it is string or numb
+            inputName = arg;
         }
     }
-
-    GetAllAssignments(page)
+    //TODO make this all terms instead
+    if(assignmentObject == undefined){
+        assignmentObject = {...await GetAllAssignments(page, T1URL), 
+            ...await GetAllAssignments(page, T2URL) }
+    }
+    //returns array of assignments unfinished probably in order
+    //TODO put this straight into the sendEmbed message after finishing console logging
+    result = await GetAssignmentForPerson(assignmentObject, inputName);
+    console.log(result)
+    // console.log(inputName)
+    SendEmbedMessage(result, message, inputName)
+    
 }
 }
 /*
@@ -85,22 +124,14 @@ async function GetAllAssignments(page, term_url="https://moodle.oeclism.catholic
     //await page.waitForSelector('table.assignment-recent')
     page.on('console', (msg) => console.log('PAGE LOG:', msg.text()));
     //THE PROBLEM WAS ASYNC, I NEED TO USE MAP OR SOMETHING TO MAKE SURE THE ARRAY PUSH STUFF IS FIXED
-    assignmentObject = await page.evaluate(() => {   
-        // Array.from(document.querySelectorAll('h3:has(> img[title="Assignment"]) > a'), element => element.textContent)
-        //Array.from(document.querySelectorAll('h3:has(> img[title="Assignment"]) > a'), element => element.textContent)
-        //Array.from(document.querySelectorAll('a'), element => element.textContent)
-        // await players.reduce(async (a, player) => {
-        //     // Wait for the previous item to finish processing
-        //     await a;
-        //     // Process this item
-        //     await givePrizeToPlayer(player);
-        //   }, Promise.resolve());
+    return await page.evaluate(() => {   
+
         
-        console.log(document.querySelector('table.assignment-recent'))
+        // console.log(document.querySelector('table.assignment-recent'))
         // allTheh3 = [].filter.call( document.querySelectorAll('h3'), function( elem ){
         //     return elem.querySelector('img[title="Assignment"]')
         // });
-        tempAssignmentObject = {}
+        AssignmentObject = {}
         for (elem of document.querySelectorAll('h3')){
             if(elem.querySelector('img[title="Assignment"]')){
                 titleString = elem.querySelector("a").textContent
@@ -126,12 +157,12 @@ async function GetAllAssignments(page, term_url="https://moodle.oeclism.catholic
                    // }
                 }
                 // console.log(titleString + " is setting people" + sibs[0])
-                tempAssignmentObject[titleString] = sibs;               
+                AssignmentObject[titleString] = sibs;               
                 // console.log(sibs)
                 
             }
         }
-        return tempAssignmentObject;
+        return AssignmentObject;
         //This is another method of geting elem, don't know what's better
         // arrayOfH3 = Array.from(document.querySelectorAll('h3'));
         // arrayOfH3.reduce(async (a, elem) => {
@@ -172,24 +203,59 @@ async function GetAllAssignments(page, term_url="https://moodle.oeclism.catholic
     });
     // #yui_3_17_2_1_1654513134108_85 > div:nth-child(6) > h3:nth-child(5) > img [alt="Assignment"]
     //In order to check if someone has done assignment I need to maybe make and object for them {t2w6 railroad: false};
-    console.log(assignmentObject)
 }
-// jQuery (optional selector filter)
-//$el.nextAll($filter);
 
-// Native (optional filter function)
-function GetNextSiblings(elem, filter) {
-        var sibs = [];
-        var nextElem = elem.parentNode.firstChild;
-        do {
-            if (nextElem.nodeType === 3) continue; // ignore text nodes
-            if (nextElem === elem) continue; // ignore elem of target
-            if (nextElem === elem.nextElementSibling) {
-                if (!filter || filter(elem)) {
-                    sibs.push(nextElem);
-                    elem = nextElem;
-                }
-            }
-        } while(nextElem = nextElem.nextSibling)
-        return sibs;
+async function GetAssignmentForPerson(assignments, personName){
+    missingAssignments = [];
+    //need a for of but for dictionary
+    // for (assignment of assignments){
+        
+    // }
+            // Object.entries(customNicknamesObj).forEach(entry => {
+        //     const [key, value] = entry;
+        //     console.log(key, value);
+        //   });
+    for(assignmentData of Object.entries(assignments)){
+        let [assignment, people] = assignmentData;
+        //If they can't be found then it pushes, meaning if you put ZZZZZ it will get all the assignments for the term
+        if(!people.some(correctPerson => correctPerson.toLowerCase().includes(personName))){
+            missingAssignments.push(assignment);
+        }
     }
+    return missingAssignments;
+}
+
+function SendEmbedMessage(missingAssignments, message, personName, title="none", colour="#156385") {
+    let embedMsg = new MessageEmbed();
+    //check if data is obj or array
+    //console.log(participantData.constructor.name);
+
+    /*
+    "Missing Assignments for $personName"
+    assignment += thing "blah blah \n"
+    */
+    if(title != "none"){
+        embedMsg.setTitle(title)
+    }
+    else{
+        embedMsg.setTitle(`Missing Assignments for ${personName}`);
+    }
+
+    let msgString = "";
+
+    for(assignment of missingAssignments){
+        // console.log(assignment)
+        if(assignment != undefined){
+            msgString += `${assignment}\n`;
+        }
+    }
+    if (msgString == ""){
+        msgString = "No Assignments Missing, Congrats :partying_face:"
+    }
+    //TODO have a field for each person or something instead
+
+    embedMsg.addField("Assignments", msgString)
+
+    embedMsg.setColor(colour);
+    message.channel.send({ embeds: [embedMsg] });
+}
